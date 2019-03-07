d8fa7d01bfb79a46d2bcdc757ce4641e5fb113ef
Avior <florian.bouillon@delta-wings.net>
2019-03-07 12:56:11 +0100

    push

--- assets/php/Functions.php
+++ assets/php/Functions.php
@@ -75,20 +75,19 @@ class Functions {
 	}
 
 	public static function deleteDir($dirPath) {
-		if (! is_dir($dirPath)) {
-			throw new InvalidArgumentException("$dirPath must be a directory");
-		}
-		if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-			$dirPath .= '/';
-		}
-		$files = glob($dirPath . '*', GLOB_MARK);
-		foreach ($files as $file) {
-			if (is_dir($file)) {
-				self::deleteDir($file);
-			} else {
-				unlink($file);
+		if (is_dir($dirPath)) {
+			if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+				$dirPath .= '/';
 			}
+			$files = glob($dirPath . '*', GLOB_MARK);
+			foreach ($files as $file) {
+				if (is_dir($file)) {
+					self::deleteDir($file);
+				} else {
+					unlink($file);
+				}
+			}
+			rmdir($dirPath);
 		}
-		rmdir($dirPath);
 	}
 }
